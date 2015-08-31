22a15baa9bac645b1ffb58fa87233143627cf671
james@jamespamplin.com <james@jamespamplin.com>
2015-08-31 13:29:31 +0100

    Fix issues with resolving server relative URLs when bundling with JSPM

--- ng-template.js
+++ ng-template.js
@@ -1,7 +1,5 @@
 /*global System:false, exports:false */
 
-var serverRelativeRegexp = /^\w+:\/\/[^\/]*/;
-
 function santiseSource( source ) {
   return source.replace( /(['\\])/g, '\\$1' )
     .replace( /[\f]/g, '\\f' )
@@ -13,22 +11,24 @@ function santiseSource( source ) {
     .replace( /[\u2029]/g, '\\u2029' );
 }
 
-function resolveUrl( address, baseUrl, useServerRelative ) {
-  if ( useServerRelative ) {
-    return address.replace( serverRelativeRegexp, '' );
-
-  } else if ( baseUrl && address.indexOf( baseUrl ) === 0 ) {
+function resolveUrl( address, baseUrl ) {
+  if ( baseUrl && address.indexOf( baseUrl ) === 0 ) {
     return address.substr( baseUrl.length );
   }
   return address;
 }
 
-exports.translate = function( load ) {
+exports.translate = function translate( load ) {
   var baseUrl = typeof System.baseURL === 'string' ? System.baseURL : '',
     options = System.ngTemplatePlugin || {},
-    url = resolveUrl( load.address, baseUrl, !!options.serverRelative );
+    url = resolveUrl( load.address, baseUrl );
 
-  return "require('angular').module('ng')" +
-    ".run(['$templateCache', function(c) { c.put('" + url + "', '" + santiseSource( load.source ) + "') }]);" +
-    "module.exports = { templateUrl: '" + url + "' };";
+  return 'var url = ' +
+    ( options.serverRelative ? "System.baseURL.replace(/^\\w+:\\/\\/[^\\/]*/,'')+" : '' ) +
+    "'" + url + "';" +
+    "require('angular').module('ng')" +
+    ".run(['$templateCache', function(c) { c.put(" +
+      "url, '" +
+      santiseSource( load.source ) + "'); }]);" +
+    'module.exports = { templateUrl: url };';
 };
